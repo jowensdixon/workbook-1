921efe4d7dbfe3bd1dc59f1dafe3b2e49f38a2d5
Jahari <jayjay8309@gmail.com>
2025-04-09 14:07:50 -0400

    completed shopping list

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        system.out.println("Banana1");
+        system.out.println("Banana2");
+        system.out.println("Banana3");
+        system.out.println("Banana4");
+        system.out.println("Banana5");
+        system.out.println("Banana6");
+        system.out.println("Banana7");
+    }
+}
+
