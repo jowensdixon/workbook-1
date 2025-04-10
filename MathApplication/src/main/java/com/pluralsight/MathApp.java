1fcc02de35f6f256dfd52fd664c3d684137ce262
Jahari <jayjay8309@gmail.com>
2025-04-10 10:40:21 -0400

    Math App

---
+++ MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+     //Question 1
+        int bobSalary = 40000;
+        int garySalary = 50000;
+        int highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println("The highest salary is ..."+ highestSalary);
+
+     //Question 2
+        int carPrice = 3000;
+        int truckPrice = 8000;
+        int smallPrice = Math.min(carPrice, truckPrice);
+        System.out.println(smallPrice);
+
+    // Question 3
+        float radius = 7.25f;
+
+
+
+
+
+
+    }
+}
